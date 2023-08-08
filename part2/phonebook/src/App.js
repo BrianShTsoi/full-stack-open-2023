32155022f2da2e20fb34d3723cd863ac119f13e2
Brian Tsoi <brian.s.tsoi@gmail.com>
2023-08-08 13:41:22 -0400

    Complete part2/phonebook step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ filterStr, handleFilterChange}) => (
   <div>
@@ -29,12 +30,18 @@ const Persons = ({ persons, filterStr }) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123 456 789'}
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [filterStr, setFilterStr] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleFilterChange = (event) => {
     setFilterStr(event.target.value)
